0d4408c0d975a3e99a13377a6f3ad9d98d29e47f
Tom Spencer <tomspencer_uk@yahoo.co.uk>
2017-11-22 15:02:57 +0000

    Adds map and midpoint calculator for website

--- index.js
+++ index.js
@@ -17,6 +17,27 @@ app.get('/', function(req, res){
   res.render('home');  //renders home.handlebars
 });
 
+app.use(function(err, req, res, next){
+  console.log('Error : ' + err.message);
+  next();
+})
+
+app.get('/about', function(req, res){
+  res.render('about');
+});
+
+app.use(function(req, res){
+  res.type('text/html');
+  res.status(404);
+  res.render('404');
+});
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.status(500);
+  res.render('500');
+})
+
 app.listen(app.get('port'), function() {   // defines what port we listne to
   console.log('Express started on http://localhost:' +
     app.get('port') + '; press Ctrl-C to terminate');
